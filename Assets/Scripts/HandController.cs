9a530040ac44cc05e9fd6d2cc9d8fb7b5c583e5c
AlexisMorvan <alexis.morvan@viacesi.fr>
2019-04-12 16:54:01 +0200

    init commit, raycast, controllers and head scripts

---
+++ Assets/Scripts/HandController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandController : MonoBehaviour
+{
+
+    public XRNode handNode;
+
+    LineRenderer lineRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = gameObject.GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 hand_pos = InputTracking.GetLocalPosition(handNode);
+        Quaternion hand_rot = InputTracking.GetLocalRotation(handNode);
+        gameObject.transform.position = hand_pos;
+        gameObject.transform.rotation = hand_rot;
+
+        
+        lineRenderer.SetPosition(0,transform.position);
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position,transform.TransformDirection(Vector3.forward), out hit, 1000))
+            lineRenderer.SetPosition(1,hit.point);
+        else {
+           lineRenderer.SetPosition(1,transform.TransformDirection(Vector3.forward) * 1000);
+        }
+    }
+}
---
+++ Assets/Scripts/HeadController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HeadController : MonoBehaviour
+{
+
+    public XRNode headNode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InputTracking.Recenter();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 head_pos = InputTracking.GetLocalPosition(headNode);
+        Quaternion head_rot = InputTracking.GetLocalRotation(headNode);
+        gameObject.transform.position = head_pos;
+        gameObject.transform.rotation = head_rot;
+    }
+}
